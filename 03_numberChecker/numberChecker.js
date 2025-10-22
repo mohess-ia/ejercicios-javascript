56ace827de698ee0bff3d0b40e0c28c56266bcc3
Mohammed Essaghraoui <121774994+mohess-ia@users.noreply.github.com>
2025-10-22 21:14:42 +0200

    He hecho los ejercicios del 1 al 3. Donde he aprendido que es una funcion.

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,25 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
+  switch(number){
+    case 1000:
+      return true;
+      break;
+
+    case 10:
+      return true;
+      break;
+
+    case 9:
+      return false;
+      break;
+
+    case 6:
+      return false;
+      break;
+    default:
+      console.log("No has introducido un numero valido.")
   }
 }
 
 // Do not edit below this line
 module.exports = numberChecker;
+
